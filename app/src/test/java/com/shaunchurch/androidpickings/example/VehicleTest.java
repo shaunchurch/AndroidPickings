dd504520cc8784d3e5ae9885aa94b2d1a173ff4b
Shaun Church <schurch@brilliantbasics.com>
2015-04-09 11:43:48 +0100

    add test stub

---
+++ app/src/test/java/com/shaunchurch/androidpickings/example/VehicleTest.java
@@ -0,0 +1,21 @@
+package com.shaunchurch.androidpickings.example;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by shaun on 08/04/15.
+ */
+public class VehicleTest extends TestCase {
+
+    public void testIncreaseSpeed() throws Exception {
+
+    }
+
+    public void testStop() throws Exception {
+
+    }
+
+    public void testGetSpeed() throws Exception {
+
+    }
+}
